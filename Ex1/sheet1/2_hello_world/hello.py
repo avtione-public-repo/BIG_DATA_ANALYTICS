3fe4353296c3774659ea3255af0a40691ff11f2d
alvinrindra <alvinrindra@gmail.com>
2017-12-29 22:13:18 +0100

    Py & R Hello World init

---
+++ Ex1/sheet1/2_hello_world/hello.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+#!/bin/sh
+print("Hello World")
+
+#yes we can execute the script from within the bash shell if we add the line env python3 on the top of the program. This tells bash to use the python3 environment for interpreting the commands and not his own.
